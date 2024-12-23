7e3a409e83c432554ba56d365758589038e215a7
Prakash Koukuntla <pkoukuntla123@gmail.com>
2024-12-23 18:02:43 +0530

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Prakash Koukuntla on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pokedexTests/pokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexTests.swift
+//  pokedexTests
+//
+//  Created by Prakash Koukuntla on 12/23/24.
+//
+
+import Testing
+@testable import pokedex
+
+struct pokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
